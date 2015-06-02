8bb2ff80a8332217b1a9bca270469b6bda94f340
user1301 <user1301@localhost.localdomain>
2015-06-02 22:54:46 +0200

    Field initialized with Cell Array.

--- Personenstrom/src/Field.java
+++ Personenstrom/src/Field.java
@@ -2,15 +2,27 @@
 public class Field {
 	
 	private int size;
-	private int gridsize;
+//	private int gridsize;
+	private Cell[][] cells;
 	
 	
 	
-	public Field(int size, int gridsize) {
+	public Field(int size/*, int gridsize*/) {
 		this.size = size;
-		this.gridsize = gridsize;
+//		this.gridsize = gridsize;
+		this.cells = new Cell[size][size]; //erzeugt Feld mit size x size Zellen
+		System.out.println(cells.length);
+		initCells(); //Feld mit leeren Zellen initialisieren
 	}
 	
+	
+	private void initCells() {
+		for(int row = 0; row < size; row++) {
+			for(int col = 0; col < size; col++) {
+				cells[row][col] = new Cell();
+			}
+		}
+	}
 	
 	
 
--- Personenstrom/src/Main.java
+++ Personenstrom/src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		Field f = new Field(10);
+
 	}
 
 }

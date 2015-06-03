c321220c7c1d1c5c28fb79a741af7edcb7f1e276
tobibi100 <tobias.hoefer@outlook.de>
2015-06-03 11:29:58 +0200

    1

--- Personenstrom/src/Field.java
+++ Personenstrom/src/Field.java
@@ -1,29 +1,25 @@
+import java.util.ArrayList;
 
 public class Field {
-	
-	private int size;
-//	private int gridsize;
-	private Cell[][] cells;
-	
-	
-	
-	public Field(int size/*, int gridsize*/) {
-		this.size = size;
-//		this.gridsize = gridsize;
-		this.cells = new Cell[size][size]; //erzeugt Feld mit size x size Zellen
-		System.out.println(cells.length);
-		initCells(); //Feld mit leeren Zellen initialisieren
-	}
-	
-	
-	private void initCells() {
-		for(int row = 0; row < size; row++) {
-			for(int col = 0; col < size; col++) {
-				cells[row][col] = new Cell();
-			}
-		}
-	}
-	
-	
+
+  private int size;
+  // private int gridsize;
+  private ArrayList<Cell> cells;
+
+  public Field(int size/* , int gridsize */) {
+    this.size = size;
+    // this.gridsize = gridsize;
+    this.cells = new ArrayList<>(); // erzeugt Feld mit size x size Zellen
+    System.out.println(cells);
+    initCells(); // Feld mit leeren Zellen initialisieren
+  }
+
+  private void initCells() {
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        cells[row][col] = new Cell();
+      }
+    }
+  }
 
 }
--- Personenstrom/src/Scheduler.java
+++ Personenstrom/src/Scheduler.java
@@ -1,4 +1,10 @@
+import java.util.PriorityQueue;
 
 public class Scheduler {
+  private PriorityQueue<Event> priorityQueue;
+
+  public Scheduler() {
+
+  }
 
 }

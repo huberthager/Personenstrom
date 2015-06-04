e1b8e6968a7a94cda21696b676d4e9323c74edc4
user1301 <user1301@localhost.localdomain>
2015-06-04 12:02:29 +0200

    nichts geändert

--- Personenstrom/src/Main.java
+++ Personenstrom/src/Main.java
@@ -1,9 +1,18 @@
+import javax.swing.JOptionPane;
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Field f = new Field(10);
-
+		int nmbrOfCells = 0;
+		String answer = JOptionPane.showInputDialog("Anzahl Zellen");
+		if(answer == null) {
+			return;
+		}
+		
+//		Field f = new Field(10);
+		FrameBuilder fbuilder = new FrameBuilder();
 	}
 	
 }

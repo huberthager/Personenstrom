e627c24536b0a85b6499f8c6b02efdedef8bfc1b
user1301 <user1301@localhost.localdomain>
2015-06-04 13:25:48 +0200

    FieldPanel CellPanel added

--- Personenstrom/src/Main.java
+++ Personenstrom/src/Main.java
@@ -1,18 +1,36 @@
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		int nmbrOfCells = 0;
-		String answer = JOptionPane.showInputDialog("Anzahl Zellen");
-		if(answer == null) {
-			return;
-		}
-		
-//		Field f = new Field(10);
-		FrameBuilder fbuilder = new FrameBuilder();
-	}
+		final FieldPanel fp = new FieldPanel("FieldPanelDemo");
+        /* Use an appropriate Look and Feel */
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+         
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                fp.createAndShowGUI();
+            }
+        });
+    }
 	
 }
